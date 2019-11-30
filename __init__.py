aa414cab1efc180e717ec429f526164b848d6dff
kizito omoit <kizitoomoit9@gmail.com>
2019-11-30 21:28:23 +0300

    Implement reading and creating of notes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
+import functools
 
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -15,10 +16,25 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    from .models import db, User
+    from .models import db, User, Note
 
     db.init_app(app)
     migrate = Migrate(app, db)
+
+    def require_login(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if not g.user:
+                return redirect(url_for('log_in'))
+            return view(**kwargs)
+        return wrapped_view
+    @app.before_request
+    def load_user():
+        user_id = session.get('user_id')
+        if user_id:
+            g.user = User.query.get(user_id)
+        else:
+            g.user = None
 
     @app.route('/sign_up', methods=('GET', 'POST'))
     def sign_up():
@@ -73,6 +89,33 @@ def create_app(test_config=None):
     @app.route('/')
     def index():
         return 'Index'
+
+    @app.route('/notes')
+    @require_login
+    def note_index():
+        return render_template('note_index.html', notes=g.user.notes)
+
+    @app.route('/notes/new', methods=('GET', 'POST'))
+    @require_login
+    def note_create():
+        if request.method =='POST':
+            title = request.form['title']
+            body = request.form['body']
+            error = None
+
+            if not title:
+                error = 'Title is required.'
+
+            if error is None:
+                note = Note(author=g.user, title=title, body=body)
+                db.session.add(note)
+                db.session.commit()
+                flash(f"Successfully created note: {title}", 'success')
+                return redirect(url_for('note_index'))
+
+            flash(error, 'error')
+
+        return render_template('note_create.html')
 
 
 
