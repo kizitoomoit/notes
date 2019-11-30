2ce0eed6f5df06aac45aec9e32964310a77d2ad4
kizito omoit <kizitoomoit9@gmail.com>
2019-12-01 00:25:56 +0300

    Add update and delete functionality for notes

--- __init__.py
+++ __init__.py
@@ -28,6 +28,11 @@ def create_app(test_config=None):
                 return redirect(url_for('log_in'))
             return view(**kwargs)
         return wrapped_view
+
+    @app.errorhandler(404)
+    def page_note_found(e):
+        return render_template('404.hmtl'), 404
+
     @app.before_request
     def load_user():
         user_id = session.get('user_id')
@@ -117,6 +122,45 @@ def create_app(test_config=None):
 
         return render_template('note_create.html')
 
+    @app.route('/notes/<note_id>/edit', methods=('GET', 'POST', 'PATCH', 'PUT'))
+    @require_login
+    def note_update(note_id):
+        note = Note.query.filter_by(user_id=g.user.id, id=note_id).first_or_404()
+
+        if request.method in ['POST', 'PATCH', 'PUT']:
+            title = request.form['title']
+            body = request.form['body']
+            error = None
+
+            if not title:
+                error = 'Title is required.'
+
+            if error is None:
+                note.title = title
+                note.body = body
+                db.session.add(note)
+                db.session.commit()
+                flash(f"Successfully updated note: {title}", 'success')
+                return redirect(url_for('note_index'))
+
+            flash(error, 'error')
+
+        return render_template('note_update.html', note=note)
+    @app.route('/notes/<note_id>/delete', methods=('GET', 'DELETE'))
+    @require_login
+    def note_delete(note_id):
+        note = Note.query.filter_by(user_id=g.user.id, id=note_id).first_or_404()
+        db.session.delete(note)
+        db.session.commit()
+        flash(f"Successfully deleted note: '{note.title}'", 'success')
+        return redirect(url_for('note_index'))
 
 
     return app
+
+
+
+
+
+
+
