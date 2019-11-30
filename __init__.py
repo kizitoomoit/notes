fea23fe0512377c56722a5bcff2789ef3c1d56cf
kizito omoit <kizitoomoit9@gmail.com>
2019-11-30 17:15:52 +0300

    Create static and template

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -14,9 +15,43 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    from .models import db
+    from .models import db, User
 
     db.init_app(app)
     migrate = Migrate(app, db)
 
+    @app.route('/sign_up', methods=('GET', 'POST'))
+    def sign_up():
+        if request.method =='POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
+
+            if not username:
+                error = 'Username is required.'
+            elif not password:
+                error = 'Password is required.'
+            elif User.query.filter_by(username=username).first():
+                error = 'Username is already taken.'
+
+            if error is None:
+                user = User(username=username, password=generate_password_hash(password))
+                db.session.add(user)
+                db.session.commit()
+                flash("Successfully signed up! Please log in.", 'success')
+                return redirect(url_for('log_in'))
+
+            flash(error, 'error')
+
+
+
+
+
+
+
+
+        return render_template('sign_up.html')
+    @app.route('/log_in')
+    def log_in():
+        return 'Login'
     return app
