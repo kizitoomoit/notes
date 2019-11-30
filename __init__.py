2f7900125525eef5543a94c5d8d19e41db0f2972
kizito omoit <kizitoomoit9@gmail.com>
2019-11-30 20:48:28 +0300

    Implement User sign Up and Authentication

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 import os
 
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -43,15 +43,37 @@ def create_app(test_config=None):
 
             flash(error, 'error')
 
-
-
-
-
-
-
-
         return render_template('sign_up.html')
-    @app.route('/log_in')
+
+    @app.route('/log_in', methods=('GET', 'POST'))
     def log_in():
-        return 'Login'
+        if request.method =='POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
+
+            user = User.query.filter_by(username=username).first()
+
+            if not user or not check_password_hash(user.password, password):
+                error = 'Username or password are incorrect.'
+
+            if error is None:
+                session.clear()
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+            flash(error, 'error')
+
+        return render_template('log_in.html')
+    @app.route('/log_out', methods=('GET', 'DELETE'))
+    def log_out():
+        session.clear()
+        flash('Successfully logged out.', 'success')
+        return redirect(url_for('log_in'))
+
+    @app.route('/')
+    def index():
+        return 'Index'
+
+
+
     return app
